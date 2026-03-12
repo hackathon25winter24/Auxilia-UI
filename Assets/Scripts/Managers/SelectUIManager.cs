c12d50440f144aa696105c8dabaf7534312d9ea1
Radon86 <aya10tomoi@gmail.com>
2026-03-12 13:17:15 +0900

    戦闘シーンの準備

--- Assets/Scripts/Managers/SelectUIManager.cs
+++ Assets/Scripts/Managers/SelectUIManager.cs
@@ -3,21 +3,17 @@ using TMPro;
 using System.Collections;
 using UnityEngine.UI;
 
-public class RoomUIManager : MonoBehaviour
+public class SelectUIManager : MonoBehaviour
 {
     public InputData inputData;
     public SceneData sceneData;
     public PlayerData playerData;
-    
     public void OnButtonClick(string buttonName)
     {
         switch (buttonName)
         {
-            case "Back":
-                sceneData.next_scene_number = 3;
-                break;
-            case "StartBattle":
-                sceneData.next_scene_number = 10;
+            case "Decided":
+                sceneData.next_scene_number = 5;
                 break;
             default:
                 Debug.Log("不明なボタン: " + buttonName);
