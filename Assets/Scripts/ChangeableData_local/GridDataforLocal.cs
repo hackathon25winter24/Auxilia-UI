fa6bdcdecf318274444a2edba2131508ea968e92
Radon86 <aya10tomoi@gmail.com>
2026-03-13 21:45:57 +0900

    攻撃の実装準備

---
+++ Assets/Scripts/ChangeableData_local/GridDataforLocal.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GridDataforLocal", menuName = "Scriptable Objects/GridDataforLocal")]
+public class GridDataforLocal : ScriptableObject
+{
+    public int[] grid_attack_position = new int[40];
+}
