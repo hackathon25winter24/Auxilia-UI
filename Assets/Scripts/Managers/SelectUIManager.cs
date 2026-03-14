18f8a26be233ca406297821b564ada33fea934ef
Radon86 <aya10tomoi@gmail.com>
2026-03-14 21:39:28 +0900

    戦闘準備シーンの整備

--- Assets/Scripts/Managers/SelectUIManager.cs
+++ Assets/Scripts/Managers/SelectUIManager.cs
@@ -8,6 +8,17 @@ public class SelectUIManager : MonoBehaviour
     public InputData inputData;
     public SceneData sceneData;
     public PlayerData playerData;
+
+    public TextMeshProUGUI party_move_cost;
+    public TextMeshProUGUI FormTime;
+    public TextMeshProUGUI start_game_text;
+    public RectTransform player_ui;
+
+    void Awake()
+    {
+        start_game_text.gameObject.SetActive(false);
+    }
+
     public void OnButtonClick(string buttonName)
     {
         switch (buttonName)
