83917ba191c535f0b8b6d7e6dfd4e74e1d3d02ac
Radon86 <aya10tomoi@gmail.com>
2026-03-15 22:08:32 +0900

    キャラクターデータの機能の拡張

--- Assets/Scripts/CosntDatas/CharacterData.cs
+++ Assets/Scripts/CosntDatas/CharacterData.cs
@@ -25,6 +25,7 @@ public class CharactersData
     [SerializeField] int _default_move_cost;
     [SerializeField] Attacks[] _attacks;
     [SerializeField] Sprite _attack_button_backimage;
+    [SerializeField] Passive[] passive;
 
     public string default_name
     {
@@ -80,6 +81,15 @@ public class Attacks
     [SerializeField] string _default_attack_name;
     [SerializeField] int _default_attack_cost;
     [SerializeField] int _default_attack_power;
+    [SerializeField] int _default_attack_target;
+    [SerializeField] int _attack_up_probability;
+    [SerializeField] int _speed_up_probability;
+    [SerializeField] int _attackcost_down_probability;
+    [SerializeField] int _poison_probability;
+    [SerializeField] int _paralysis_probability;
+    [SerializeField] int _speed_down_probability;
+    [SerializeField] int _attackcost_up_probability;
+    [SerializeField] int _bleeding_probability;
     [SerializeField] Sprite _attack_button;
     [SerializeField] Vector2Int[] _default_attack_range;
 
@@ -98,6 +108,51 @@ public class Attacks
         get{return _default_attack_power;}
     }
 
+    public int default_attack_target
+    {
+        get{return _default_attack_target;}
+    }
+
+    public int attack_up_probability
+    {
+        get{return _attack_up_probability;}
+    }
+
+    public int speed_up_probability
+    {
+        get{return _speed_up_probability;}
+    }
+
+    public int attackcost_down_probability
+    {
+        get{return _attackcost_down_probability;}
+    }
+
+    public int poison_probability
+    {
+        get{return _poison_probability;}
+    }
+
+    public int paralysis_probability
+    {
+        get{return _paralysis_probability;}
+    }
+
+    public int speed_down_probability
+    {
+        get{return _speed_down_probability;}
+    }
+    
+    public int attackcost_up_probability
+    {
+        get{return _attackcost_up_probability;}
+    }
+    
+    public int bleeding_probability
+    {
+        get{return _bleeding_probability;}
+    }
+    
     public Sprite attack_button
     {
         get { return _attack_button; }
@@ -106,5 +161,26 @@ public class Attacks
     public Vector2Int[] default_attack_range
     {
         get{return _default_attack_range;}
+    }
+}
+
+[System.Serializable]
+public class Passive
+{
+    [SerializeField] int _passive_power;
+    [SerializeField] int _passive_target;
+    [SerializeField] Vector2Int[] _passive_range;
+
+    public int passive_power
+    {
+        get{return _passive_power;}
+    }
+    public int passive_target
+    {
+        get{return _passive_target;}
+    }
+    public Vector2Int[] passive_range
+    {
+        get{return _passive_range;}
     }
 }
