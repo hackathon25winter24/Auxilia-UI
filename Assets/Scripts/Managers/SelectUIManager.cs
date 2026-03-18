a1b40dae493981f7ce547535253364f43861bc09
Radon86 <aya10tomoi@gmail.com>
2026-03-18 21:14:18 +0900

    戦闘準備のプレイヤー視点をつくりました

--- Assets/Scripts/Managers/SelectUIManager.cs
+++ Assets/Scripts/Managers/SelectUIManager.cs
@@ -12,10 +12,18 @@ public class SelectUIManager : MonoBehaviour
     public BattleDataforOmline battleDataforOnline;
 
     public TextMeshProUGUI party_move_cost;
-    public TextMeshProUGUI FormTime;
     public TextMeshProUGUI start_game_text;
     public TextMeshProUGUI timertext;
     public RectTransform player_ui;
+
+    public Image[] SelecuUI;
+    public GameObject characterTub;
+    public GameObject roomButtonPrefab; 
+    public Transform contentParent;    
+    public Image characterUI;
+    
+    public int selectedUI;
+    public int selectedCharacterId;
 
     public float maxTime = 100f; 
     private float currentTime;
@@ -23,12 +31,12 @@ public class SelectUIManager : MonoBehaviour
 
     void Awake()
     {
+        battleDataforOnline.selected_character[0] = playerData.character_formation_one;
+        battleDataforOnline.selected_character[1] = playerData.character_formation_two;
+        battleDataforOnline.selected_character[2] = playerData.character_formation_three;
+        characterTub.SetActive(false);
         start_game_text.gameObject.SetActive(false);
-        battleDataforOnline.all_move_cost
-        = characterData.characters[battleDataforOnline.selected_character[0]].default_move_cost
-        + characterData.characters[battleDataforOnline.selected_character[1]].default_move_cost
-        + characterData.characters[battleDataforOnline.selected_character[2]].default_move_cost;
-        party_move_cost.text = "cost : " + battleDataforOnline.all_move_cost;
+        UpDateCharacterUI();
         TimerStart();
     }
 
@@ -36,14 +44,42 @@ public class SelectUIManager : MonoBehaviour
     {
         switch (buttonName)
         {
+            case "Select1":
+                selectedUI = 0;
+                characterTub.SetActive(true);
+                CharacterButtons();
+                break;
+            case "Select2":
+                selectedUI = 1;
+                characterTub.SetActive(true);
+                CharacterButtons();
+                break;
+            case "Select3":
+                selectedUI = 2;
+                characterTub.SetActive(true);
+                CharacterButtons();
+                break;
             case "Decided":
                 sceneData.next_scene_number = 5;
+                break;
+            case "BackShadow":
+                characterTub.SetActive(false);
                 break;
             default:
                 Debug.Log("不明なボタン: " + buttonName);
                 break;
         }
     }
+
+    public void CharacterClick(int ButtonNum)
+    {
+        battleDataforOnline.selected_character[selectedUI] = ButtonNum;
+        characterTub.SetActive(false);
+        UpDateCharacterUI();
+    }
+
+    public void CharacterLongClick(int LongButtonNum)
+    {}
 
     void Update()
     {
@@ -68,5 +104,46 @@ public class SelectUIManager : MonoBehaviour
     {
         currentTime = maxTime;
         isTimerRunning = true;
+    }
+
+    void UpDateCharacterUI()
+    {
+        battleDataforOnline.all_move_cost
+        = characterData.characters[battleDataforOnline.selected_character[0]].default_move_cost
+        + characterData.characters[battleDataforOnline.selected_character[1]].default_move_cost
+        + characterData.characters[battleDataforOnline.selected_character[2]].default_move_cost;
+        party_move_cost.text = "cost : " + battleDataforOnline.all_move_cost;
+        for (int i = 0; i <= 2; i++)
+        {
+            SelecuUI[i].sprite = characterData.characters[battleDataforOnline.selected_character[i]].select_image;
+        }
+    }
+
+    public void CharacterButtons()
+    {
+        // 既存のリストを一度クリア（二重生成防止）
+        foreach (Transform child in contentParent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < characterData.characters.Length; i++)
+        {
+            characterUI.sprite = characterData.characters[i].select_image;
+            GameObject newButton = Instantiate(roomButtonPrefab, contentParent);
+
+            // ボタンが押された時の処理をコードから登録
+            int buttonIndex = i; 
+
+            RoomButtonLongPress longPressScript = newButton.GetComponent<RoomButtonLongPress>();
+            if (longPressScript != null)
+            {
+            longPressScript.myIndex = buttonIndex;
+            // 長押しされた時に実行するメソッドを登録
+            longPressScript.onLongPressWithIndex.AddListener(CharacterLongClick);
+            }
+
+            newButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => CharacterClick(buttonIndex));
+        }
     }
 }
